7b2d06bbf63fa4c595bb4ecc75fe9881d3af4f46
x-rexen <eywv3828@gmail.com>
2026-05-25 13:04:09 +0800

    Create caccount.js

---
+++ scripts/caccount.js
@@ -0,0 +1,16 @@
+function checkhave(ph,pn){
+    var un=localStorage.getItem('un');
+    if(un){
+        var pw=localStorage.getItem('pw');
+        findRowWhere(un,pw,'account').then(row=>{
+            if(row!==-1){if(pn!=null) window.location.href=pn;}
+            else{
+                localStorage.removeItem('un');
+                localStorage.removeItem('pw');
+                localStorage.removeItem('pc');
+                localStorage.removeItem('n');
+                if(ph!=null) window.location.href=ph;
+            }
+        });
+    }else if(ph!=null) window.location.href=ph;
+}
