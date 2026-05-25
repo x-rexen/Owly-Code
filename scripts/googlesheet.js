30c7b15b713d97e0cf355cf8e4efa913447e1770
x-rexen <eywv3828@gmail.com>
2026-05-25 12:44:58 +0800

    Update googlesheet.js

--- scripts/googlesheet.js
+++ scripts/googlesheet.js
@@ -2,20 +2,19 @@ var CONFIG={
     scriptUrl:'https://script.google.com/macros/s/AKfycbxIbOkIn48_nRDPEfQ69PuR8WoeU-jLm8FiQg5Uy26tgk9M9iPuvay8UK4vhEyrN87fPw/exec',
     sheetId:'1dOvHsJxH47L0eId6bwaX-jZ_rm2O5bAFzR71M9VUmYM'
 };
-
-async function findRowWhere(col1Matches,col2Matches,sheetName){
+async function findRowWhere(col1,col2,name){
     try{
         const response=await fetch(CONFIG.scriptUrl,{
             method:'POST',
             body:new URLSearchParams({
                 action:'getAll',
                 sheetId:CONFIG.sheetId,
-                sheetName:sheetName
+                sheetName:name
             })
         });
         const data=await response.json();
         for(let i=0;i<data.length;i++){
-            if(data[i][0]==col1Matches&&data[i][1]==col2Matches) return i+1;
+            if(data[i][0]==col1&&data[i][1]==col2) return i+1;
         }
         return-1;
     }catch(error){
@@ -24,14 +23,14 @@ async function findRowWhere(col1Matches,col2Matches,sheetName){
     }
 }
 
-async function readCell(row,col,sheetName){
+async function readCell(row,col,name){
     try{
         const response=await fetch(CONFIG.scriptUrl,{
             method:'POST',
             body:new URLSearchParams({
                 action:'read',
                 sheetId:CONFIG.sheetId,
-                sheetName:sheetName,
+                sheetName:name,
                 row:row,
                 col:col
             })
@@ -44,14 +43,14 @@ async function readCell(row,col,sheetName){
     }
 }
 
-async function writeCell(row,col,value,sheetName){
+async function writeCell(row,col,value,name){
     try{
         const response=await fetch(CONFIG.scriptUrl,{
             method:'POST',
             body:new URLSearchParams({
                 action:'write',
                 sheetId:CONFIG.sheetId,
-                sheetName:sheetName,
+                sheetName:name,
                 row:row,
                 col:col,
                 value:value
@@ -65,19 +64,19 @@ async function writeCell(row,col,value,sheetName){
     }
 }
 
-async function writeNewRow(rowData,sheetName){
+async function writeNewRow(rowData,name){
     try{
         const response=await fetch(CONFIG.scriptUrl,{
             method:'POST',
             body:new URLSearchParams({
                 action:'getAll',
                 sheetId:CONFIG.sheetId,
-                sheetName:sheetName
+                sheetName:name
             })
         });
         const data=await response.json();
         const newRow=data.length+1;
-        for(let i=0;i<rowData.length;i++) await writeCell(newRow,i+1,rowData[i],sheetName);
+        for(let i=0;i<rowData.length;i++) await writeCell(newRow,i+1,rowData[i],name);
         return newRow;
     }catch(error){
         console.error('Error writing new row:',error);
@@ -85,14 +84,14 @@ async function writeNewRow(rowData,sheetName){
     }
 }
 
-async function getAllData(sheetName){
+async function getAllData(name){
     try{
         const response=await fetch(CONFIG.scriptUrl,{
             method:'POST',
             body:new URLSearchParams({
                 action:'getAll',
                 sheetId:CONFIG.sheetId,
-                sheetName:sheetName
+                sheetName:name
             })
         });
         const data=await response.json();
@@ -102,7 +101,4 @@ async function getAllData(sheetName){
         return[];
     }
 }
-
-async function changeCell(row,col,newValue,sheetName){
-    return await writeCell(row,col,newValue,sheetName);
-}
+async function changeCell(row,col,newValue,name){return await writeCell(row,col,newValue,name);}
